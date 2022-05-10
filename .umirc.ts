0c353f8bffd845cf2e145e83a543a0c50c78483d
ranshaw <493060037@qq.com>
2022-05-10 14:04:03 +0800

    doc: 修改文档

--- .umirc.ts
+++ .umirc.ts
@@ -10,20 +10,7 @@ export default defineConfig({
   mode: 'site',
   // more config: https://d.umijs.org/config
   alias: {
-    // "@alifd":  path.resolve(__dirname, 'src/RuleTree/packages/@alifd'),
-    // "@aligov":  path.resolve(__dirname, 'src/RuleTree/packages/@aligov'),
-    // "@sula":  path.resolve(__dirname, 'src/RuleTree/packages/@sula'),
-    // "d3-hierarchy": path.resolve(__dirname, 'src/RuleTree/packages/d3-hierarchy'),
-    // "dayjs":  path.resolve(__dirname, 'src/RuleTree/packages/dayjs'),
-    // "bignumber.js": path.resolve(__dirname, 'src/RuleTree/packages/bignumber.min.js'),
-    // "lodash.clonedeep": path.resolve(__dirname, 'src/RuleTree/packages/lodash.clonedeep'),
-    // "moment": path.resolve(__dirname, 'src/RuleTree/packages/moment'),
-    // "rc-field-form":  path.resolve(__dirname, 'src/RuleTree/packages/rc-field-form'),
-    // "async-validator": path.resolve(__dirname, 'src/RuleTree/packages/async-validator'),
-    // "react-dnd": path.resolve(__dirname, 'src/RuleTree/packages/react-dnd'),
-    // "react-dnd-html5-backend": path.resolve(__dirname, 'src/RuleTree/packages/react-dnd-html5-backend'),
-    // "dnd-core": path.resolve(__dirname, 'src/RuleTree/packages/dnd-core'),
-    // "react-lifecycles-compat": path.resolve(__dirname, 'src/RuleTree/packages/react-lifecycles-compat'),
+
   },
   extraBabelPlugins: [
     [
